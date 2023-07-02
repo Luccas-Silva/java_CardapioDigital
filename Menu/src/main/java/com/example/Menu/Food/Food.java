a6f1ddc2db27f0a97897a04b3e57db61d5533b90
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2023-07-02 15:17:33 -0300

    back-end, front-end finished

--- Menu/src/main/java/com/example/Menu/Food/Food.java
+++ Menu/src/main/java/com/example/Menu/Food/Food.java
@@ -1,13 +1,23 @@
 package com.example.Menu.Food;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "foods")
 @Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Food {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +25,53 @@ public class Food {
 	
 	private String title;
 	private String image;
-	private Integer price; 
+	private Integer price;
 	
+	public Food() {
+	}
+	
+	public Food(Long id, String title, String image, Integer price) {
+		this.id = id;
+		this.title = title;
+		this.image = image;
+		this.price = price;
+	}
+	
+	public Food(FoodRequestDTO data) {
+		this.title = data.title();
+		this.image = data.image();
+		this.price = data.price();
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public Integer getPrice() {
+		return price;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Food other = (Food) obj;
+		return Objects.equals(id, other.id);
+	}
 }
--- Menu/src/main/java/com/example/Menu/controller/FoodController.java
+++ Menu/src/main/java/com/example/Menu/controller/FoodController.java
@@ -3,12 +3,17 @@ package com.example.Menu.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Menu.Food.Food;
 import com.example.Menu.Food.FoodRepository;
+import com.example.Menu.Food.FoodRequestDTO;
+import com.example.Menu.Food.FoodResponseDTO;
 
 @RestController
 @RequestMapping("food")
@@ -17,10 +22,19 @@ public class FoodController {
 	@Autowired
 	private FoodRepository repository;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@PostMapping
+	public void saveFood(@RequestBody FoodRequestDTO data) {
+		Food foodData = new Food(data);
+		repository.save(foodData);
+		return;
+	}
+	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
-	public List<Food> getAll() {
+	public List<FoodResponseDTO> getAll() {
 		
-		List<Food> foodlist = repository.findAll();
+		List<FoodResponseDTO> foodlist = repository.findAll().stream().map(FoodResponseDTO::new).toList();
 		return foodlist;
 	}
 }
